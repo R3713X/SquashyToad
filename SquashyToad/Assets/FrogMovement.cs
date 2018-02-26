23650d7561182c5ecc517f100377e22cc11dafea
R3713X <kapsalas_gi@yahoo.gr>
2018-02-26 21:48:26 +0200

    Initial commit

---
+++ SquashyToad/Assets/FrogMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrogMovement : MonoBehaviour {
+    public int jumpFactor =5;
+    public int jumpDegree=45;
+    public float jumpGroundClearance = 1;
+    public float speedTolerance=5;
+    
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bool isOnGround = Physics.Raycast(transform.position, -transform.up, jumpGroundClearance);
+        Debug.DrawRay(transform.position, -transform.up * jumpGroundClearance);
+        var speed = GetComponent<Rigidbody>().velocity.magnitude;
+        bool isNearStationary = speed < speedTolerance;
+        if ( GvrViewer.Instance.Triggered && isOnGround && isNearStationary)
+        {
+            var camera = GetComponentInChildren<Camera>();
+            Vector3 projectedLookVector = Vector3.ProjectOnPlane(camera.transform.forward, Vector3.up);
+            float realJumpDergree = Mathf.Deg2Rad * jumpDegree;
+            Vector3 jumpVector = Vector3.RotateTowards((projectedLookVector.normalized) * jumpFactor, Vector3.up, realJumpDergree, 0);
+            GetComponent<Rigidbody>().AddForce(jumpVector, ForceMode.VelocityChange);
+            //GetComponent<Rigidbody>().velocity = jumpVector;
+        }
+	}
+}
---
+++ SquashyToad/Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextScene()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentIndex+1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        print("Game will quit");
+    }
+}
