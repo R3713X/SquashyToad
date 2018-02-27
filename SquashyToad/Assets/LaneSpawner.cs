9807988bf572d26ac18c4e6e32b00ac7460d1e9b
R3713X <kapsalas_gi@yahoo.gr>
2018-02-27 07:07:09 +0200

    Added Random Lane spawning Random Tree spawning, Scaled everything 5 times bigger so it feels like you have a tiny frog

---
+++ SquashyToad/Assets/LaneSpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaneSpawner : MonoBehaviour {
+
+    public GameObject[] lanePrefabs;
+
+	// Use this for initialization
+	void Start () {
+        int offset = 0;
+        while (offset<500)
+        {
+            CreateRandomLane(offset);
+            offset += 50;
+        }
+
+    }
+	
+    void CreateRandomLane(float offset)
+    {
+        int laneIndex = Random.Range(0,lanePrefabs.Length);
+        var lane = Instantiate(lanePrefabs[laneIndex]);
+        lane.transform.parent = transform;
+        lane.transform.Translate(0, 0, offset);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
